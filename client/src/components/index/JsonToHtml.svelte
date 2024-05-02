1c951ac8b2fbaa76d8bae680c1dda68dac20c7de
freemancho1 <freeman.cho@gmail.com>
2024-05-02 17:41:45 +0900

    office2405021750

---
+++ client/src/components/index/JsonToHtml.svelte
@@ -0,0 +1,30 @@
+<script>
+    import JsonToHtmlSub from "./JsonToHtmlSub.svelte";
+    export let jsonList;
+    // $: jsonList1 = JSON.parse(jsonList)
+    const temp = Object.entries(jsonList);
+    
+    let isObject = (obj) => typeof obj === 'object' && obj !== null;
+    // const isObject = (obj) => {
+    //     return typeof obj === 'object' && obj !== null;
+    // }
+</script>
+
+<li class="list-group-item">&#123;</li>
+<ul class="list-group list-group-flush">
+    {#each temp as [key, value]}
+        {#if isObject(value)}
+            <li class="list-group-item sub-item-t">{key}: &#123;</li>
+            <li class="list-group-item sub-item">
+                <JsonToHtmlSub jsonData={value} isObject={isObject} />
+            </li>
+        {:else}
+            <li class="list-group-item sub-item-t">
+                <div class="d-flex justify-content-between">
+                    <div>{key}:</div><div>{value}</div>
+                </div>
+            </li>
+        {/if}
+    {/each}
+</ul>
+<li class="list-group-item">&#125;</li>
--- client/src/components/index/Main.svelte
+++ client/src/components/index/Main.svelte
@@ -1,5 +1,6 @@
 <script>
     import ButtonGroup from "./ButtonGroup.svelte";
+    import JsonToHtml from "./JsonToHtml.svelte";
 
     const getSamples = () => {
         console.log('Click getSamples');
@@ -15,12 +16,53 @@
         {id: "bbb", color: "btn-primary", click: reqPredict, label: "Req Predict"},
         {id: "aaa", color: "btn-primary", click: reqRePredict, label: "reqRe Predict"},
     ]
+    let jsonData = {
+        "title": "Get Samples", 
+        "color": "btn-success", 
+        "click": "getSamples()",
+        "getSampleBtn": {
+            "title": "Get Samples", 
+            "color": "btn-success", 
+            "click": "getSamples()",
+            "reqPredictBtn": {
+                "title": "Req Predict", 
+                "color": "btn-danger", 
+                "click": "reqPredict()",
+                "reqPredictBtn": {
+                    "title": "Req Predict", 
+                    "color": "btn-danger", 
+                    "click": "reqPredict()"
+                },
+                "reqRePredictBtn": {
+                    "title": "ReReq Predict", 
+                    "color": "btn-danger", 
+                    "click": "reqRePredict()"
+                },
+            },
+            "reqRePredictBtn": {
+                "title": "ReReq Predict", 
+                "color": "btn-danger", 
+                "click": "reqRePredict()"
+            },
+        },
+        "reqPredictBtn": {
+            "title": "Req Predict", 
+            "color": "btn-danger", 
+            "click": "reqPredict()"
+        },
+        "reqRePredictBtn": {
+            "title": "ReReq Predict", 
+            "color": "btn-danger", 
+            "click": "reqRePredict()"
+        },
+    };
+    $: jsonList = jsonData;
 </script>
 
 <div class="row justify-content-around">
     <!-- Input JSON data -->
     <div class="col-4">
-        <button class="btn btn-primary">Primary</button>
+        <JsonToHtml {jsonList} />
     </div>
     <!-- Action Buttons -->
     <div class="col-2">
@@ -31,3 +73,19 @@
         <button class="btn btn-success">Success</button>
     </div>
 </div>
+
+<style>
+    :global(.sub-item-t) {
+        padding-left: 30px;
+    }
+    :global(.sub-item-b) {
+        padding-left: 0px;
+        /* border: 0px !important; */
+    }
+    :global(.sub-item) {
+        padding-left: 30px;
+        padding-top: 0px;
+        padding-bottom: 0px;
+        padding-right: 0px;
+    }
+</style>
